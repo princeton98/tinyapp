2e3a445470bac6a48e2e143e44a272236401868d
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-11 21:31:39 +0000

    made a few changes to express server, added a shorten link to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -20,6 +20,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
