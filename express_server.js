da529488a78bd59c89350dd965eb050080b14a66
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-12 00:00:24 +0000

    added new function, added new formatting through urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+function generateRandomString() {
+  // 6 random alphanumeric characters
+  let arr = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  let randomNumorLetter = []
+  for (let i = 0; i < 6; i++) {
+    randomNumorLetter.push(arr[Math.floor(Math.random() * 63)]);  
+  }
+  finalString = randomNumorLetter.join("")
+ return finalString;
+};
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,6 +35,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -32,6 +49,8 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: newShortURL, longURL: urlDatabase[newShortURL]};
   res.render("urls_show", templateVars);
 });
+
+console.log(generateRandomString());
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
