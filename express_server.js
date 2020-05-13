8418afa4d9dbc8e6fbbbde82f056e30c6eb9aa78
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-13 01:26:19 +0000

    added new post function for logging out, and updated json files

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,7 +20,7 @@ function generateRandomString() {
     randomNumorLetter.push(arr[Math.floor(Math.random() * 63)]);  
   }
   finalString = randomNumorLetter.join("")
- return finalString;
+  return finalString;
 };
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -32,21 +34,44 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/login", (req, res) => {
+  //console.log(req.body);
+  res.cookie("username", req.body.username)
+  //console.log(req.cookies["username"]);
+  res.redirect("/urls");
+})
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("urls");
+})
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {}
+     templateVars = {
+      urls: urlDatabase,
+      username: req.cookies["username"]
+     }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL
   let newShortURL = shortURL.slice(1); //can shorten this
-  let templateVars = { shortURL: newShortURL, longURL: urlDatabase[newShortURL] };
+  let templateVars = { 
+    shortURL: newShortURL, 
+    longURL: urlDatabase[newShortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
+
 app.post("/urls", (req, res) => {
   // console.log(req.body);
   randomString = generateRandomString();
