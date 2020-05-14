3fdd6f69a24d74328a5649824fd3281e9dfd59a7
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-14 18:19:14 +0000

    added features that restricted outside users from deleting/editing URLs

--- express_server.js
+++ express_server.js
@@ -45,6 +45,25 @@ function lookUp(newEmail) {
   return false;
 }
 
+function urlsForUser(id) {
+  let URLs = []
+  let keys = Object.keys(urlDatabase)
+  //if (id === cookie("user_id")) {
+    for (let i = 0; i < keys.length; i++) {
+      if (urlDatabase[keys[i]].userID === id) {
+        let tempURLs = {}
+        tempURLs.shortURL = keys[i];
+        tempURLs.longURL = urlDatabase[keys[i]].longURL;
+        URLs.push(tempURLs);
+        //console.log("hello");
+      }
+    }
+  //}
+  //console.log(urlDatabase.length);
+  Object.assign(URLs);
+  return URLs;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -75,13 +94,13 @@ app.post("/login", (req, res) => {
 
 app.get("/login", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["id"]]
+    user: users[req.cookies["user_id"]]
   }
   res.render("login", templateVars);
 })
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("urls");
+  res.redirect("login");
 })
 
 app.get("/register", (req, res) => {
@@ -110,12 +129,16 @@ app.post("/register", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    res.end("Please login or register first");
+  } else {
      let templateVars = {
-      urls: urlDatabase,
-      user: users[req.cookies["user_id"]]
-     }
-     //console.log(users[req.cookies["id"]]);
+       urls: urlsForUser(req.cookies["user_id"]),
+       user: users[req.cookies["user_id"]]
+      }
+      //console.log(urlsForUser(req.cookies["user_id"]));
   res.render("urls_index", templateVars);
+}
 });
 
 app.get("/urls/new", (req, res) => {
@@ -132,40 +155,59 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL
   let newShortURL = shortURL.slice(1); //can shorten this
+      if (!req.cookies["user_id"]) {
+        res.end("Not logged in");
+      }
+      else if (urlDatabase[newShortURL].userID !== req.cookies["user_id"]) {
+        res.end("URL locked");
+      } else {
   let templateVars = { 
     shortURL: newShortURL, 
-    longURL: urlDatabase[newShortURL],
+    longURL: urlDatabase[newShortURL].longURL,
     user: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
-});
+}});
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body);
+   console.log(req.body);
   randomString = generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
+  urlDatabase[randomString] = {longURL: req.body.longURL, userID: req.cookies["user_id"]}
+  //console.log(urlDatabase);
     res.redirect(`/urls/:${randomString}`);
 })
 app.post("/urls/:id", (req, res) => {
   let sliceURL = req.params.id.slice(1);
-  urlDatabase[sliceURL] = req.body.updatedURL;
+  if (!req.cookies["user_id"]){
+    res.end("You are not logged in")
+  } else if (urlDatabase[sliceURL].userID !== req.cookies["user_id"]) {
+    res.end("You cannot delete this link");
+  } else {
+  urlDatabase[sliceURL] = {longURL:req.body.updatedURL, userID: req.cookies["user_id"]};
+  //console.log(req.body.updatedURL)
   res.redirect("/urls");
-})
+}})
 
 app.post(`/urls/:shortURL/delete`, (req, res) => {
   let shortURL = req.params.shortURL;
   let sliceURL = shortURL.slice(1);
+  if (!req.cookies["user_id"]){
+    res.end("You are not logged in")
+  } else if (urlDatabase[sliceURL].userID !== req.cookies["user_id"]) {
+    res.end("You cannot delete this link");
+  } else {
   delete urlDatabase[sliceURL];
   res.redirect ("/urls");
-});
+}});
 
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL].longURL
   res.redirect(longURL);
 })
 
 //console.log(generateRandomString());
+//console.log(urlsForUser("aJ481W"));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
